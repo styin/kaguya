be0847724da3c92f4d9ba36897b36e491cc7773e
Sebastian Tianci Yin <sebastian.ytc@outlook.com>
2026-04-08 22:13:15 +0800

    fix: removed hardcoded path

--- gateway/build.rs
+++ gateway/build.rs
@@ -1,13 +1,10 @@
+/// Compiles the proto files to Rust code using tonic_build. 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(target_os = "windows")]
-    if std::env::var("PROTOC").is_err() {
-        // 改成你自己的 protoc 路径
-        std::env::set_var("PROTOC", r"D:\protoc-34.0-win64\bin\protoc.exe");
-    }
+    // Ensure that PROTOC is available in the environment.
 
     tonic_build::configure()
-        .build_server(true)   // Gateway 作为 server: ListenerService, RouterControlService
-        .build_client(true)   // Gateway 作为 client: TalkerService, ReasonerService
+        .build_server(true)   // Gateway as server: ListenerService, RouterControlService
+        .build_client(true)   // Gateway as client: TalkerService, ReasonerService
         .compile_protos(
             &["../proto/kaguya/v1/kaguya.proto"],
             &["../proto"],
